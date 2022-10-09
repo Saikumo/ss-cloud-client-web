97d9bc98894ad41b8feceaf28435a1c60655eba8
Xufuhao <saikumo11@gmail.com>
2022-10-09 14:32:04 +0800

    [Create] add fileTable

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import {router} from "./router/index.js";
+import ElementPlus from "element-plus"
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus)
+app.use(router);
+app.mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-table :data="fileTableData">
+    <el-table-column prop="fileName" label="文件名" />
+    <el-table-column prop="modifiedDateTime" label="修改时间" />
+    <el-table-column prop="fileSize" label="文件大小" />
+  </el-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fileTableData: [{
+        fileName: "1",
+        modifiedDateTime: "222",
+        fileSize: 231,
+      }],
+    };
+  },
+  method: {
+    
+  },
+};
+</script>
+
+<style></style>
