9c61a8adc8681505398a163ccf7b0f615d6a515a
Xufuhao <saikumo11@gmail.com>
2022-10-18 16:58:34 +0800

    [Create] add list file feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <el-button @click="refreshFileTableData()">刷新</el-button>
+  <el-button @click="refreshFileTable()">刷新</el-button>
+  <el-button @click="back()">返回上一级目录</el-button>
 
-  <el-table :data="fileTableData">
+  <el-table :data="fileTableData" @cell-dblclick="handleCellDoubleClick">
+    <el-table-column type="selection" width="55" />
     <el-table-column prop="fileName" label="文件名" min-width="80%" />
-    <el-table-column prop="lastModifiedTime" label="上次修改时间" min-width="20%" />
+    <el-table-column
+      prop="lastModifiedTime"
+      label="上次修改时间"
+      min-width="20%"
+    />
     <el-table-column prop="fileSize" label="文件大小" min-width="20%" />
   </el-table>
 </template>
@@ -14,27 +20,36 @@ import axios from "axios"
 export default {
   data() {
     return {
-      fileTableData: [
-        {
-          fileName: "1",
-          modifiedDateTime: "222",
-          fileSize: 231,
-        },
-      ],
+      fileTableData: [],
+      filePathArr: [],
     }
   },
   methods: {
-    refreshFileTableData() {
+    refreshFileTable() {
       axios
         .get("/api/file/", {
           params: {
-            path: "D:/Programming/Project/ss-cloud-server/src/main/resources/static",
+            filePath: "/" + this.filePathArr.join("/"),
           },
         })
         .then((res) => {
           this.fileTableData = res.data.data
         })
     },
+    handleCellDoubleClick(row, column, cell, event) {
+      // refresh fileTable if it is a directory
+      if (row.isDirectory) {
+        this.filePathArr = row.filePath.split("/")
+        this.refreshFileTable()
+      }
+    },
+    back() {
+      this.filePathArr.pop()
+      this.refreshFileTable()
+    },
+  },
+  mounted() {
+    this.refreshFileTable()
   },
 }
 </script>
